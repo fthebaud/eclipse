440ed5bdea6584a84208f41d8baaf9c797f9bb68
Fabien Thebaud <fthebaud@gmail.com>
2016-08-31 21:34:17 +0200

    adding Angular, WIP

---
+++ app/app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+//define the eclipse module
+angular.module('eclipse', []);
---
+++ app/tile/tile.component.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// retreiving the eclipse module
+angular.module('eclipse')
+  // registering a tile component
+  .component('tile', {
+    templateUrl: 'app/tile/tile.template.html',
+    controller: function tileController() {
+    }
+  });
